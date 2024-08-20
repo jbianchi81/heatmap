442eaf19f6f3d7fed0e7641319b4dd66f148cee5
jbianchi81 <jbianchi@ina.gob.ar>
2024-08-20 16:31:03 -0300

    prono density

--- app/heatmap.js
+++ app/heatmap.js
@@ -157,7 +157,7 @@ internal.heatmap = class {
             }
         })
     }
-	pronoDensityVector (timestart,timeend,dt="1 day",filter={},options={}) {
+	async pronoDensityVector (timestart,timeend,dt="1 day",filter={},options={}) {
 		if(!timestart) {
 			timestart = new Date()
 			timestart = new Date(timestart.getFullYear(),timestart.getMonth(),timestart.getDay())
@@ -170,7 +170,36 @@ internal.heatmap = class {
 		if(new Date(timestart).toString() == "Invalid Date" || new Date(timeend).toString() == "Invalid Date" || !dt) {
 			return Promise.reject("Missing or bad parameters: timestart,timeend,dt")
 		}
-		var valid_filters= {"var_id": {table:"series",type:"integer"},"tabla_id":{table:"redes",type:"string"},"unit_id":{table:"series",type:"integer"},"estacion_id":{table:"series",type:"integer"},"proc_id":{table:"series",type:"integer"},cal_id:{table:"corridas_last",type:"integer"}}
+		var valid_filters= {
+            "var_id": {type:"integer"},
+            "unit_id":{type:"integer"},
+            "proc_id":{type:"integer"},
+            cal_id:{table:"series_prono_date_range_last",type:"integer"}
+        }
+        if(filter.tipo == "areal") {
+            var prono_table =  "pronosticos_areal"
+            valid_filters.fuentes_id = {type:"integer"}
+            valid_filters.area_id = {type:"integer"}
+            var series_table = "series_areal"
+            var extra_joins = "" // ["areas_pluvio","fuentes"]
+        } else if (filter.tipo == "raster" || filter.tipo == "rast") {
+            var prono_table = "pronosticos_rast"
+            valid_filters.fuentes_id = {table:"series_areal",type:"integer"}
+            valid_filters.escena_id = {type:"integer"}
+            var series_table = "series_rast"
+            var extra_joins = ""
+        } else {
+            var prono_table = "pronosticos"
+            valid_filters.tabla_id = {table:"redes",type:"string"}
+            valid_filters.estacion_id = {type:"integer"}
+            var series_table = "series"
+            var extra_joins = `
+                JOIN estaciones 
+                    ON (series.estacion_id = estaciones.unid)
+                JOIN redes
+                    ON (estaciones.tabla = redes.tabla_id)
+            `
+        }
 		var count_filters = 0
 		Object.keys(valid_filters).forEach(key=>{
 			if(filter[key]) {
@@ -179,60 +208,65 @@ internal.heatmap = class {
 		})
 		// console.log({count_filters:count_filters})
 		if(count_filters == 0) {
-			return Promise.reject("Missing at least one filter: var_id, proc_id, unit_id, tabla_id, estacion_id, cal_id")
+			return Promise.reject("Missing at least one filter: " + Object.keys(valid_filters).join(", "))
 		}
-		var filter_string = control_filter2(valid_filters,filter)
-		var query  = "WITH ts as (\
-			SELECT generate_series($1::timestamp,$2::timestamp,$3::interval) t\
-			),\
-			tseries as (\
-			SELECT t,\
-				   row_number() OVER (order by t) x\
-			FROM ts\
-			),\
-			prono AS (\
-			SELECT pronosticos.timestart,\
-				   corridas_last.id\
-			FROM pronosticos,corridas_last,series,estaciones,redes\
-			WHERE pronosticos.timestart >= $1::timestamp\
-			AND pronosticos.timestart < $2::timestamp\
-			AND pronosticos.series_id = series.id\
-			AND pronosticos.cor_id = corridas_last.id\
-			AND series.estacion_id = estaciones.unid\
-			AND estaciones.tabla = redes.tabla_id\
-			" + filter_string + ")\
-			SELECT tseries.x,\
-				   tseries.t,\
-				   count(prono.timestart)\
-			FROM tseries\
-			LEFT JOIN prono ON (prono.timestart >= tseries.t\
-			AND prono.timestart < tseries.t+$3::interval)\
-			GROUP BY x,t\
-			ORDER BY x;"
+        
+		var filter_string = control_filter2(valid_filters,filter, series_table)
+		var query  = `WITH ts as (
+			SELECT generate_series($1::timestamp,$2::timestamp,$3::interval) t
+			),
+			tseries as (
+			SELECT t,
+				   row_number() OVER (order by t) x
+			FROM ts
+			),
+			prono AS (
+                SELECT 
+                    ${prono_table}.timestart
+                FROM ${prono_table}
+                JOIN series_prono_date_range_last
+                    ON (${prono_table}.cor_id = series_prono_date_range_last.cor_id)
+                JOIN ${series_table} 
+                    ON (${prono_table}.series_id = ${series_table}.id)
+                ${extra_joins}
+                WHERE ${prono_table}.timestart >= $1::timestamp
+                AND ${prono_table}.timestart < $2::timestamp
+                ${filter_string}
+            )
+			SELECT tseries.x,
+				   tseries.t,
+				   count(prono.timestart)
+			FROM tseries
+			LEFT JOIN prono ON (prono.timestart >= tseries.t
+			AND prono.timestart < tseries.t+$3::interval)
+			GROUP BY x,t
+			ORDER BY x`
 		var query_string = pasteIntoSQLQuery(query,[timestart,timeend,dt])
-		console.log(query_string)
-		return this.pool.query(query_string) // query,[timestart,timeend,dt])
-		.then(result=>{
-			if (!result.rows) {
-				throw("Query error")
-			}
-			var values = result.rows.map(row=>row.count)
-			if(options.format && options.format == "tuples") {
-				return result.rows.map(row=>{
-					return [row.x, row.t, row.count]
-				})
-			}
-			if(options.include_timestamps) {
-				return {
-					values: values,
-					timestamps: result.rows.map(row=>row.t)
-				}
-			} else {
-				return {
-					values: values
-				}
-			}
-		})
+		console.debug(query_string)
+        try {
+    		var result = await this.pool.query(query_string) // query,[timestart,timeend,dt])
+        } catch(e) {
+            throw(new Error(e))
+        }
+        if (!result.rows) {
+            throw(new Error("Query error"))
+        }
+        var values = result.rows.map(row=>row.count)
+        if(options.format && options.format == "tuples") {
+            return result.rows.map(row=>{
+                return [row.x, row.t, row.count]
+            })
+        }
+        if(options.include_timestamps) {
+            return {
+                values: values,
+                timestamps: result.rows.map(row=>row.t)
+            }
+        } else {
+            return {
+                values: values
+            }
+        }
 	}
 	obsArealDensityVector (timestart,timeend,dt="1 day",filter={},options={}) {
         if(!timestart) {
